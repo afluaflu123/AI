34658a2767435e623dc5150cc08eb837af580e53
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-09-09 18:56:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hacker-Jr-TG/K-bot.git /Team-KL
+  git clone https://github.com/afluaflu123/AI.git /AI
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Team-KL
+  git clone $UPSTREAM_REPO /AI
 fi
-cd /Team-KL
+cd /AI
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
